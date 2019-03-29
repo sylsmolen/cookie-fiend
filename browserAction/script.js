5273d891ff2d1353396942d594ef5e8f4a7192cc
sylsmolen <sylwester.smolen@wp.pl>
2019-03-29 21:14:13 +0000

    set and get browser storage from browser action window

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,1 +1,42 @@
-document.getElementById('myHeading').style.color = 'red'
+const COOKIE_FIEND = "COOKIE_FIEND";
+const setlocalStorageBtn = document.querySelector("#setLocalStorageBtn");
+const getlocalStorageBtn = document.querySelector("#getLocalStorageBtn");
+
+const setItem = () => {
+  console.log("sync storage item set");
+};
+
+const getItem = storageItem => {
+  console.log("storage item: ", JSON.stringify(storageItem));
+};
+
+const onSetStorageError = err => {
+  console.log("set sync storage error", err);
+};
+
+const onGetStorageError = err => {
+  console.log("get sync storage error", err);
+};
+
+const setSyncStorage = value => {
+  browser.storage.sync
+    .set({ [COOKIE_FIEND]: value })
+    .then(setItem, onSetStorageError);
+};
+
+const getSyncStorage = value => {
+  browser.storage.sync.get("COOKIE_FIEND").then(getItem, onGetStorageError);
+};
+
+const setLSBtnClikced = e => {
+  const textField = document.querySelector("#lsTxtArea");
+
+  if (textField.value) {
+    console.log(textField.value);
+    setSyncStorage(textField.value);
+  }
+
+  console.log("clicked");
+};
+setlocalStorageBtn.addEventListener("click", setLSBtnClikced);
+getlocalStorageBtn.addEventListener("click", getSyncStorage);
