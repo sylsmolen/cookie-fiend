797d68bbb0561c38ce73244d7129bfa53ac223a7
sylsmolen <sylwester.smolen@wp.pl>
2019-03-31 11:05:22 +0100

    getting active tab url in browserAction

--- browserAction/script.js
+++ browserAction/script.js
@@ -48,3 +48,16 @@ const setLSBtnClikced = e => {
 };
 setlocalStorageBtn.addEventListener("click", setLSBtnClikced);
 getlocalStorageBtn.addEventListener("click", getSyncStorage);
+
+function logTabs(tabs) {
+  for (let tab of tabs) {
+    console.log(tab.url);
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+var querying = browser.tabs.query({ currentWindow: true, active: true });
+querying.then(logTabs, onError);
