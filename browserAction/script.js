dadf81b126d7a4d692cfd5c604d22c2df72ab7c0
sylsmolen <sylwester.smolen@wp.pl>
2019-04-04 19:17:49 +0100

    fix 'getConfigForCurrentDomain' func

--- browserAction/script.js
+++ browserAction/script.js
@@ -3,7 +3,6 @@ const COOKIE_FIEND = "COOKIE_FIEND";
 const compose = (...fns) => (...args) =>
   fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
 const logErr = txt => err => console.error(txt, err);
-const log = txt => console.log(txt);
 const head = arr => arr[0];
 const getProp = prop => obj => obj[prop];
 const thunk = func => data => () => func(data);
@@ -14,22 +13,19 @@ const inspect = value => {
 };
 
 const unary = fn => arg => fn(arg);
-const unaryJsonParser = unary(JSON.parse);
-
 const parseTwice = compose(
-  // unaryJsonParser,
-  unaryJsonParser
+  JSON.parse,
+  JSON.parse
 );
 
 /* BROWSER STORAGE */
 
 const getStorageItemName = domain => `${COOKIE_FIEND}::${domain}`;
 
-const getItem = domain => async storageItem => {
-  console.log(storageItem);
+const getItem = key => async storageItem => {
   try {
-    const eventConfig = parseTwice(storageItem[getStorageItemName(domain)]);
-    console.log(eventConfig);
+    const eventConfig = parseTwice(storageItem[key]);
+    console.log("eventConfig", eventConfig);
   } catch (err) {
     logErr("get item failed")(err);
   }
@@ -43,17 +39,15 @@ const setSyncStorage = valueThunk => async key => {
     .set({
       [key]: unpacked
     })
-    .then(log, logErr("set storage failed"));
+    .then(inspect, logErr("set storage failed"));
   console.log("res", res);
   console.log("key", key);
 
   return res;
 };
 
-const getSyncStorage = domain =>
-  browser.storage.sync
-    .get(getStorageItemName(domain))
-    .then(getItem(domain), logErr("getSyncStorage"));
+const getSyncStorage = key =>
+  browser.storage.sync.get(key).then(getItem(key), logErr("getSyncStorage"));
 
 const getTextAreaFieldValue = () => {
   const textField = document.querySelector("#lsTxtArea");
@@ -95,9 +89,7 @@ const setHref = setAttribute("href");
 const setHrefOnLink = setHref(createLinkElement);
 
 const getDomainFromUrl = compose(
-  inspect,
   getProp("hostname"),
-  inspect,
   setHrefOnLink
 );
 
@@ -109,10 +101,14 @@ const setConfigForCurrentDomain = async () => {
     getDomainFromUrl
   )(tabUrl);
 };
-const getConfigForCurrentDomain = compose(
-  getSyncStorage,
-  getActiveTabUrl
-);
+const getConfigForCurrentDomain = async () => {
+  const tabUrl = await getActiveTabUrl();
+  compose(
+    getSyncStorage,
+    getStorageItemName,
+    getDomainFromUrl
+  )(tabUrl);
+};
 
 /* EVENTS */
 
