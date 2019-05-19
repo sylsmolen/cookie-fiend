fb2b0dc6cd5b9c1a67b1b5df84534cbd4000e894
sylsmolen <sylwester.smolen@wp.pl>
2019-05-19 14:43:16 +0100

    add reason react

--- src/backgroundScripts/index.js
+++ src/backgroundScripts/index.js
@@ -1,32 +1,32 @@
-import "@babel/polyfill";
-import { getActiveTabAsync } from "../shared/tabs";
-import { composePromise } from "../utils";
-console.log("hello from the barckround script");
+import '@babel/polyfill'
+import { getActiveTabAsync } from '../shared/tabs'
+import { composePromise } from '../utils'
+console.log('hello from the barckround script')
 
 var executeContentScript = tab =>
   browser.tabs.executeScript(tab.id, {
-    file: "/content_scripts/index.js"
-  });
+    file: '/content_scripts/index.js'
+  })
 
-const run = composePromise(executeContentScript, getActiveTabAsync);
+const run = composePromise(executeContentScript, getActiveTabAsync)
 
 const onTabUpdated = async (tabId, changeInfo, tab) => {
   if (changeInfo.url) {
-    console.log("url change", changeInfo.url);
-    executeContentScript(tab);
+    console.log('url change', changeInfo.url)
+    executeContentScript(tab)
   }
-};
+}
 
-browser.tabs.onUpdated.addListener(onTabUpdated);
+browser.tabs.onUpdated.addListener(onTabUpdated)
 
-run();
+run()
 
 var createData = {
-  type: "detached_panel",
-  url: "/extension_page/index.html",
+  type: 'detached_panel',
+  url: '/extension_page/index.html',
   width: 250,
   height: 100
-};
-var creating = browser.windows.create(createData);
+}
+var creating = browser.windows.create(createData)
 
-console.log("creating: ", creating);
+console.log('creating: ', creating)
---
+++ src/extensionPage/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const outputDir = path.join(__dirname, 'build/')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './lib/js/src/Index.bs.js',
+  mode: isProd ? 'production' : 'development',
+  output: {
+    path: outputDir,
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: false
+    }),
+    new CopyWebpackPlugin([{ from: './public', to: '' }])
+  ],
+  devServer: {
+    compress: true,
+    contentBase: outputDir,
+    port: process.env.PORT || 8000,
+    historyApiFallback: true
+  }
+}
